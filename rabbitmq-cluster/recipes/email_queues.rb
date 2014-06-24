2fcff801468450b16c3cc95f8e95da1776f75c45
richard2753 <richard.i.wilkinson@gmail.com>
2014-06-24 10:24:55 +0100

    added email queues recipe

---
+++ rabbitmq-cluster/recipes/email_queues.rb
@@ -0,0 +1,34 @@
+# Add all rabbitmq nodes to the hosts file with their short name.
+instances = node[:opsworks][:layers][:rabbitmq][:instances]
+
+rabbit_nodes = instances.map{ |name, attrs| "rabbit@#{name}" }
+node.set['rabbitmq']['cluster_disk_nodes'] = rabbit_nodes
+
+include_recipe 'rabbitmq'
+
+rabbitmq_user "guest" do
+  action :delete
+end
+
+rabbitmq_user node['rabbitmq_cluster']['user'] do
+  password node['rabbitmq_cluster']['password']
+  action :add
+end
+
+rabbitmq_user node['rabbitmq_cluster']['user'] do
+  vhost "/"
+  permissions ".* .* .*"
+  action :set_permissions
+end
+
+rabbitmq_user node['rabbitmq_cluster']['user'] do
+  tag "administrator"
+  action :set_tags
+end
+
+rabbitmq_policy "ha-two" do
+  pattern "^(?!amq\\.).*"
+  params {"ha-mode" => "exactly","ha-params" => 2, "ha-sync-mode" => "automatic"}
+  priority 1
+  action :set
+end
